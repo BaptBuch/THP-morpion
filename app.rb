5fd8494171cc4046a6c1ae8e81ece52506168cc3
Bapt <baptiste.buch@gmail.com>
2021-04-30 08:26:13 +0200

    app.rb

--- app.rb
+++ app.rb
@@ -1,25 +1,28 @@
-require 'pry'
-
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'Board'
 require 'BoardCase'
 require 'Game'
 require 'Player'
 require 'Show'
+require 'bundler'
+Bundler.require
 
-my_game = Game.new#(player1, player2)
+class Application
 
-my_game.lets_go
+  my_game = Game.new
 
-while my_game.status == "Ongoing"
-  
-  my_game.turn
-  
-  
-  
-  
+  my_game.lets_go
 
-  
+  while my_game.status == "Ongoing"
+    
+    my_game.turn
+    
+    
+    
+    
+
+    
+  end
+  my_game.game_end(my_game.lets_go)
+
 end
-my_game.game_end(my_game.lets_go)
-#binding.pry 
